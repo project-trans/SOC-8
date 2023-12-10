10fdcff0a95d507271ec926f097026fc49d50a22
Rizumu Ayaka <rizumu@ayaka.moe>
2023-12-10 18:33:20 +0800

    feat: custom containers

---
+++ docs/.vitepress/theme/components/Containers.vue
@@ -0,0 +1,15 @@
+<template>
+<!-- <div class="mt-12 pt-6 w-full border-(0 solid gray-800) dark:border-gray-400" /> -->
+<div
+  class="
+    suggestion-statement
+    border border-solid border-gray-800 dark:border-gray-400
+    my-14
+    pt-4 px-4
+    selector-[.suggestion-statement>h2]:(mt-0 pt-0 border-none)
+    selector-[.suggestion-statement_.header-anchor]:(top-0! left-5px!)
+  "
+>
+  <slot />
+</div>
+</template>
