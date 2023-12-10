0031a65711265c9becc23a95b50469edc0a910e4
Rizumu Ayaka <rizumu@ayaka.moe>
2023-12-10 18:45:56 +0800

    feat: sort menus by frontmatter order

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -8,7 +8,8 @@ export const sidebar =  generateSidebar([
     scanStartPath: 'soc8cn',
     resolvePath: '/soc8cn/',
     useTitleFromFrontmatter: true,
+    sortMenusByFrontmatterOrder: true,
   },
-  
+
   // 这个 `as` 源于 vitepress-sidebar 的类型定义与实际情况的差异，目前不影响使用，后续 vitepress-sidebar 修复后可以移除。
 ]) as DefaultTheme.Config['sidebar']
