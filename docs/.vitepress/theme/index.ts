0df2b15295b45f80feeb23517b92373a03a304cc
北雁 Cryolitia <Cryolitia@gmail.com>
2023-12-12 20:03:05 +0800

    read enhance

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,11 +7,31 @@ import './styles/vitepress-vars.css'
 import './styles/styles.css'
 import 'uno.css'
 
+import {  
+  NolebaseHighlightTargetedHeading,  
+} from '@nolebase/vitepress-plugin-highlight-targeted-heading'
+
+import '@nolebase/vitepress-plugin-highlight-targeted-heading/dist/style.css'
+
+import { 
+  NolebaseEnhancedReadabilitiesMenu, 
+  NolebaseEnhancedReadabilitiesScreenMenu, 
+} from '@nolebase/vitepress-plugin-enhanced-readabilities'
+
+import '@nolebase/vitepress-plugin-enhanced-readabilities/dist/style.css'
+
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
+      // 为较宽的屏幕的导航栏添加阅读增强菜单
+      'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu), 
+      // 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
+      'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu),
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'layout-top': () => [ 
+        h(NolebaseHighlightTargetedHeading), 
+      ], 
     })
   },
   enhanceApp() {
