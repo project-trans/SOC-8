582d6186eff2333b4db95295878922be78327ae7
Neko Ayaka <neko@ayaka.moe>
2024-02-19 17:47:57 +0700

    chore: apply @nolebase/vitepress-plugin-git-changelog plugin
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,17 +3,38 @@ import { defineConfig } from 'vite'
 import { MarkdownTransform } from './.vitepress/plugins/markdownTransform'
 import Components from 'unplugin-vue-components/vite'
 import UnoCSS from 'unocss/vite'
+import { GitChangelog, GitChangelogMarkdownSection } from '@nolebase/vitepress-plugin-git-changelog/vite'
 
 export default defineConfig({
   plugins: [
     MarkdownTransform(),
+    GitChangelog({
+      maxGitLogCount: 2000,
+        repoURL: () => 'https://github.com/project-trans/SOC-8',
+        rewritePaths: {
+          'docs/': 'SOC-8/',
+        },
+    }),
+    GitChangelogMarkdownSection({
+      sections: {
+        disableChangelog: false,
+        disableContributors: true,
+      },
+      getChangelogTitle: () => '变更历史',
+      excludes: [],
+      exclude: (_, { helpers }): boolean => {
+        if (helpers.idEquals('index.md'))
+          return true
+
+        return false
+      },
+    }),
     Components({
       dirs: resolve(__dirname, '.vitepress/theme/components'),
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: './.vitepress/components.d.ts',
       transformer: 'vue3',
     }),
-
     UnoCSS(),
   ],
 })
